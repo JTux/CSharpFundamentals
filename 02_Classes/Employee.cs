04228d1817cc8e8b5d879c3dde4bbb1fb6d1c4f1
Joshua Tucker <thejltucker@gmail.com>
2019-04-11 10:07:06 -0400

    Added Employee and EmployeeTests

---
+++ 02_Classes/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Classes
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int YearsOfService { get; set; }
+
+        public Employee() { }
+        public Employee(int id, string lastName, int age, int yearsOfService)
+        {
+            ID = id;
+            LastName = lastName;
+            Age = age;
+            YearsOfService = yearsOfService;
+        }
+    }
+}
---
+++ 02_Classes_Tests/EmployeeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using _02_Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _02_Classes_Tests
+{
+    [TestClass]
+    public class EmployeeTests
+    {
+        [TestMethod]
+        public void EmployeePropertyTest()
+        {
+            Employee employee = new Employee(1, "Tucker", 24, 1);
+            Assert.AreEqual(1, employee.ID);
+            Assert.AreEqual("Tucker", employee.LastName);
+            Assert.AreEqual(24, employee.Age);
+            Assert.AreEqual(1, employee.YearsOfService);
+        }
+    }
+}
