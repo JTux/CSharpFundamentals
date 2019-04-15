bf3c7f689324b3abf5d50146469c0e68058e03a7
Joshua Tucker <thejltucker@gmail.com>
2019-04-15 11:41:45 -0400

    Added Calculator Challenge Repository and Tests

---
+++ 04_Calculator_Data/CalculatorRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Calculator_Data
+{
+    public class CalculatorRepository
+    {
+        /// <summary>
+        /// Calculates and returns the sum of the two double parameters.
+        /// </summary>
+        /// <param name="numOne">First number.</param>
+        /// <param name="numTwo">Second number, added to the first.</param>
+        /// <returns>Sum of numOne and numTwo.</returns>
+        public double Add(double numOne, double numTwo)
+        {
+            double sum = numOne + numTwo;
+            return sum;
+        }
+
+        /// <summary>
+        /// Calculates and returns the sum all values in the given List.
+        /// </summary>
+        /// <param name="numbers">List of all numbers to be added together.</param>
+        /// <returns></returns>
+        public double Add(List<double> numbers)
+        {
+            double sum = 0;
+
+            foreach(double number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Calculates and returns the difference between the two double parameters.
+        /// </summary>
+        /// <param name="numOne">First number.</param>
+        /// <param name="numTwo">Second number, subtracted to the first.</param>
+        /// <returns>Difference between numOne and numTwo.</returns>
+        public double Subtract(double numOne, double numTwo)
+        {
+            double diff = numOne - numTwo;
+            return diff;
+        }
+
+        /// <summary>
+        /// Calculates and returns the product of the two double parameters.
+        /// </summary>
+        /// <param name="numOne">First number, to be multiplied by the second.</param>
+        /// <param name="numTwo">Second number, to be multiplied by the first.</param>
+        /// <returns>Product of numOne and numTwo.</returns>
+        public double Multiply(double numOne, double numTwo)
+        {
+            double prod = numOne * numTwo;
+            return prod;
+        }
+
+        /// <summary>
+        /// Calculates and returns the quotient from dividing the dividend by the divisor.
+        /// </summary>
+        /// <param name="dividend">First number, the dividend.</param>
+        /// <param name="divisor">Second number, the divisor.</param>
+        /// <returns>Quotient from dividing the dividend by the divisor.</returns>
+        public double Divide(double dividend, double divisor)
+        {
+            double quotient = dividend / divisor;
+            return quotient;
+        }
+    }
+}
---
+++ 04_Calculator_Tests/CalculatorRepositoryTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using _04_Calculator_Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _04_Calculator_Tests
+{
+    [TestClass]
+    public class CalculatorRepositoryTests
+    {
+        [TestMethod]
+        public void CalcRepo_AddTwoNumbers_ShouldReturnCorrectDouble()
+        {
+            // Arrange
+            CalculatorRepository repo = new CalculatorRepository();
+
+            // Act
+            double expected = repo.Add(7, 5);
+            double actual = 12;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcRepo_AddListOfNumbers_ShouldReturnCorrectDouble()
+        {
+            // Arrange
+            CalculatorRepository repo = new CalculatorRepository();
+            List<double> numberList = new List<double>();
+            numberList.Add(7d);
+            numberList.Add(2d);
+            numberList.Add(3d);
+            numberList.Add(4d);
+            numberList.Add(3.25d);
+            numberList.Add(1d);
+
+            // Act
+            double expected = 20.25;
+            double actual = repo.Add(numberList);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcRepo_SubtractTwoNumbers_ShouldReturnCorrectDouble()
+        {
+            // Arrange
+            CalculatorRepository repo = new CalculatorRepository();
+
+            // Act
+            double expected = repo.Subtract(7, 5);
+            double actual = 2;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcRepo_MultiplyTwoNumbers_ShouldReturnCorrectDouble()
+        {
+            // Arrange
+            CalculatorRepository repo = new CalculatorRepository();
+
+            // Act
+            double expected = repo.Multiply(7, 5);
+            double actual = 35;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalcRepo_DivideTwoNumbers_ShouldReturnCorrectDouble()
+        {
+            // Arrange
+            CalculatorRepository repo = new CalculatorRepository();
+
+            // Act
+            double expected = repo.Divide(12.5, 5);
+            double actual = 2.5;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
